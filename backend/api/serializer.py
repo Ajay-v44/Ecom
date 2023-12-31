748388ee8dad40ea762f4dd6574aa07a71fff239
Ajay v <vajay4834@gmail.com>
2023-12-31 15:20:08 +0530

    serializer and user views completed

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -57,3 +57,28 @@ class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model=OrderItem
         fields="__all__"
+class OrderSerializer(serializers.ModelSerializer):
+    orderItems = serializers.SerializerMethodField(read_only=True)
+    shippingAddress = serializers.SerializerMethodField(read_only=True)
+    User = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+    def get_orderItems(self,obj):
+        items = obj.orderitem_set.all()
+        serializer = OrderItemSerializer(items,many=True)
+        return serializer.data
+
+    def get_shippingAddress(self,obj):
+        try:
+            address = ShippingAddressSerializer(obj.shippingaddress,many=False).data
+        except:
+            address = False
+        return address
+
+    def get_User(self,obj):
+        items = obj.user
+        serializer = UserSerializer(items,many=False)
+        return serializer.data
