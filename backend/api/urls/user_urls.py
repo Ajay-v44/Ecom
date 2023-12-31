c02542ef71d24ef47bfadac96a85e686828ec056
Ajay v <vajay4834@gmail.com>
2023-12-31 17:55:19 +0530

    backend completed

--- backend/api/urls/user_urls.py
+++ backend/api/urls/user_urls.py
@@ -3,9 +3,9 @@ from api.views import user_views as views
 urlpatterns = [
     path('register/',views.registerUser,name='register'),
     path('profile/',views.getUserProfile,name='user_profile'),
-     path('profile/update/',views.updatedUserProfile,name='user_profile'),
-      path('login/',views.MyTokenObtainPairView.as_view(),name='tokenlogin'),
-      path('update/<str:pk>/',views.updateUser,name="updateUser"),
+     path('profile/update/',views.updateUserProfile,name='user_profile'),
+      path('login/',views.MyTokenObtainPairViews.as_view(),name='login'),
+    #   path('update/<str:pk>/',views.updateUser,name="updateUser"),
       path('delete/<str:pk>/',views.deleteUser,name="deleteUser"),
 
 
